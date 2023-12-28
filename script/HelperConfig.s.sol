46893a3b6e833ad4340443b02c306efede127843
LouisF <base216ball@yahoo.com>
2023-12-28 15:41:49 -0300

    first commit

---
+++ script/HelperConfig.s.sol
@@ -0,0 +1,57 @@
+//SPDX-License-Identifier: MIT
+//Deploy mocks when on local anvil chain
+//Keep track of contract addresses across different chains
+
+pragma solidity ^0.8.18;
+
+import {Script} from "forge-std/Script.sol";
+import {MockV3Aggregator} from '../test/mocks/MockV3Aggregator.sol';
+
+contract HelperConfig is Script {
+    NetworkConfig public activeNetworkConfig;
+
+    uint8 public constant DECIMALS = 8;
+    int256 public constant INITIAL_PRICE = 2000e8;
+
+    struct NetworkConfig {
+        address priceFeed; //ETH/USD price feed address
+    }
+
+    constructor() {
+        if (block.chainid == 11155111) {
+            //gets the different block ids and sets active network to right chain
+            activeNetworkConfig = getSepoliaEthConfig();
+        } else if (block.chainid == 1) {
+            activeNetworkConfig = getMainnetEthConfig();
+        } else {
+            activeNetworkConfig = getOrCreateAnvilEthConfig();
+        } //add more chains as needed
+    }
+
+    function getSepoliaEthConfig() public pure returns (NetworkConfig memory) {
+        NetworkConfig memory sepoliaConfig = NetworkConfig({
+            priceFeed: 0x694AA1769357215DE4FAC081bf1f309aDC325306
+        });
+        return sepoliaConfig;
+    }
+
+    function getOrCreateAnvilEthConfig() public returns (NetworkConfig memory) {
+        //needs mocks because contracts feeds aren't deployed on anvil local chain
+        if(activeNetworkConfig.priceFeed != address(0)){ //if we already set up pricefeed we return;
+            return activeNetworkConfig;
+        }
+        vm.startBroadcast();
+        MockV3Aggregator mockPriceFeed = new MockV3Aggregator(DECIMALS, INITIAL_PRICE);
+        vm.stopBroadcast();
+
+        NetworkConfig memory anvilConfig = NetworkConfig({priceFeed : address(mockPriceFeed)});
+        return anvilConfig;
+    }
+
+    function getMainnetEthConfig() public pure returns (NetworkConfig memory) {
+        NetworkConfig memory ethConfig = NetworkConfig({
+            priceFeed: 0x5f4eC3Df9cbd43714FE2740f5E3616155c5b8419
+        });
+        return ethConfig;
+    }
+}
